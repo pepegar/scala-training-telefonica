2cf7e2fd18ea79d5fa52df6e4d4d5b73d4f10cf2
Pepe García <pepe@pepegar.com>
2018-10-16 16:53:07 +0200

    2.2 solution

--- exercises/exercise2/app/main/scala/model.scala
+++ exercises/exercise2/app/main/scala/model.scala
@@ -1,7 +1,11 @@
 package exercise2
 
+import play.api.libs.json.{Json, Reads}
+
 object model {
 
   case class Car(brand: String, model: String)
+
+  implicit val carReads: Reads[Car] = Json.reads[Car]
 
 }
