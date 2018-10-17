b7778ac3a532e4e3fa8eec7738b93bee36c0d0fe
Pepe García <pepe@pepegar.com>
2018-10-17 10:51:34 +0200

    exercise 3 solution

--- exercises/exercise3/app/main/scala/Controller.scala
+++ exercises/exercise3/app/main/scala/Controller.scala
@@ -3,12 +3,51 @@ package exercise3
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import model._
+import play.api.libs.json.{JsError, Json, Reads}
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class Controller @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index() = Action {
-    Ok("hello")
+  var users: List[User] = List()
+
+  def listAll() = Action(Ok(Json.toJson(users)))
+
+
+  def create() = Action(validateAs[User]) { implicit req =>
+    users = req.body :: users
+
+    Ok("")
   }
-  
+
+  def read(id: Int) = Action {
+    users.find(_.id == id) match {
+      case None => NotFound(s"user with id $id not found")
+      case Some(x) => Ok(Json.toJson(x))
+    }
+  }
+
+  def update(Id: Int) = Action(validateAs[User]) { implicit req =>
+    val user = req.body
+
+    users = users map {
+      case User(Id, name) => user
+      case x => x
+    }
+
+    Ok("")
+  }
+
+  def delete(id: Int) = Action { implicit req =>
+    users = users.dropWhile(_.id == id)
+
+    Ok("")
+  }
+
+
+  private def validateAs[A : Reads] = parse.json.validate(
+    _.validate[A].asEither.left.map(e => BadRequest(JsError.toJson(e)))
+  )
 }
--- exercises/exercise3/app/main/scala/model.scala
+++ exercises/exercise3/app/main/scala/model.scala
@@ -1,5 +1,11 @@
 package exercise3
 
+import play.api.libs.json.{Json, Reads, Writes}
+
 object model {
 
+  case class User(id: Int, name: String)
+
+  implicit val readsUser: Reads[User] = Json.reads[User]
+  implicit val writesUser: Writes[User] = Json.writes[User]
 }
