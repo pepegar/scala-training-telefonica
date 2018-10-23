36f7b46ab19b8cf1f1b26e70ddd2accaaf89aeb9
Pepe García <pepe@pepegar.com>
2018-10-23 15:13:41 +0200

    exercise 6 solution

--- exercises/exercise6/src/main/scala/Main.scala
+++ exercises/exercise6/src/main/scala/Main.scala
@@ -1,10 +1,53 @@
 package exercise5
 
-object Main extends App {
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+
+import scala.io.StdIn
+
+object Main extends App with Model {
+
+  var users: List[User] = List()
 
   /**
     * Exercise 6. Create a CRUD service that for users.  Users have an ID and a name
     */
+  val routes =
+    path("/") {
+      get {
+        complete(users)
+      } ~
+      put {
+        entity(as[User]) { user =>
+          users = user :: users
+
+          complete("")
+        }
+      }
+    } ~
+    pathPrefix("/", IntNumber) { Id =>
+      get {
+        complete(users.find(_.id == Id))
+      } ~ post {
+        entity(as[User]) { user =>
+          val IntId = Id.asInstanceOf[Int]
+          users = users map {
+            case User(IntId, _) => user
+            case x => x
+          }
+          complete("")
+        }
+      } ~ delete {
+        users = users.dropWhile(_.id == Id)
+        complete("")
+      }
+    }
+
+
   implicit val system = ActorSystem("exercise-6")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
@@ -17,5 +60,10 @@ object Main extends App {
     .flatMap(_.unbind()) // trigger unbinding from the port
     .onComplete(_ => system.terminate()) // and shutdown when done
 
+}
 
+trait Model extends DefaultJsonProtocol with SprayJsonSupport {
+
+  case class User(id: Int, name: String)
+  implicit val userFormat: RootJsonFormat[User] = jsonFormat2(User)
 }
