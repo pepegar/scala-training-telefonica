fec46d3c49eedf12fa61ca10c7ba8da3e68e3aa2
Pepe García <pepe@pepegar.com>
2018-10-23 10:54:45 +0200

    exercise 1.3 solution

--- exercises/exercise1/src/main/scala/tree.scala
+++ exercises/exercise1/src/main/scala/tree.scala
@@ -3,3 +3,6 @@ package exercise1
 /**
   * exercise 1.3 create a binary tree structure
   */
+sealed trait Tree[A]
+case class Empty[A]() extends Tree[A]
+case class Node[A](l: Tree[A], a: A, r: Tree[A]) extends Tree[A]
