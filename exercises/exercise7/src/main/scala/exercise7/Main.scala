fc78e5c9857d2423eeb3534cebe275a480344540
Pepe Garcia <pepe@pepegar.com>
2018-11-27 02:57:25 +0100

    exercise 7 solution

--- exercises/exercise7/src/main/scala/exercise7/Main.scala
+++ exercises/exercise7/src/main/scala/exercise7/Main.scala
@@ -34,6 +34,18 @@ object Main extends App {
 
   val system = ActorSystem("exercise7")
 
+  val room = system.actorOf(Room.props("developers"), "developers-room")
+
+  val pepe = system.actorOf(Member.props("pepe"), "pepe")
+  val juan = system.actorOf(Member.props("juan"), "juan")
+  val antonio = system.actorOf(Member.props("antonio"), "antonio")
+
+  room ! Join(pepe)
+  room ! Join(juan)
+  room ! Join(antonio)
+
+  room ! Message(pepe, "mensaje de pepe")
+
   system.terminate()
 
 }
