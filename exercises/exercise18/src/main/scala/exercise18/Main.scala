b3641f1d1fe353501e24405d40778a6721d27c8b
Pepe García <pepe@pepegar.com>
2018-11-30 04:02:18 +0100

    exercise 18 solution

--- exercises/exercise18/src/main/scala/exercise18/Main.scala
+++ exercises/exercise18/src/main/scala/exercise18/Main.scala
@@ -1,9 +1,13 @@
 package exercise18
 
 import akka.actor.ActorSystem
+import akka.kafka.ProducerSettings
+import akka.kafka.scaladsl.Producer
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Compression, Framing, Sink, StreamConverters}
 import akka.util.ByteString
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -12,8 +16,11 @@ object Main extends App {
 
   implicit val system = ActorSystem("akka-streams")
   implicit val mat = ActorMaterializer()
-
-
+  val bootstrapServers = "localhost:9092"
+  val producerConf = system.settings.config.getConfig("akka.kafka.producer")
+  val producerSettings =
+    ProducerSettings(producerConf, new StringSerializer, new StringSerializer)
+      .withBootstrapServers(bootstrapServers)
   /**
     * Create a producer (represented as a Sink in alpakka-kafka) and make it send
     * all the records to the random-data topic in kafka
@@ -24,7 +31,8 @@ object Main extends App {
     .via(Compression.gunzip())
     .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 300))
     .map(_.utf8String)
-    .runWith(???)
+    .map(value => new ProducerRecord[String, String]("random-data", value))
+    .runWith(Producer.plainSink(producerSettings))
 
   Await.result(flow, Duration.Inf)
 
