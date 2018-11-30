d838c830da41eb688ef01709e177881c1675139a
Pepe García <pepe@pepegar.com>
2018-11-30 04:24:17 +0100

    exercise 19 solution

--- exercises/exercise19/src/main/scala/exercise19/Main.scala
+++ exercises/exercise19/src/main/scala/exercise19/Main.scala
@@ -1,13 +1,15 @@
 package exercise19
 
 import akka.actor.ActorSystem
-import akka.kafka.ProducerSettings
-import akka.kafka.scaladsl.Producer
+import akka.kafka.{ConsumerSettings, ProducerSettings, Subscriptions}
+import akka.kafka.scaladsl.{Consumer, Producer}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Compression, Framing, Sink, StreamConverters}
 import akka.util.ByteString
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -21,6 +23,20 @@ object Main extends App {
   /**
     * Create a consumer that consumes messages from the random-data topic in kafka and prints them all
     */
-  
+  val consumerConf = system.settings.config.getConfig("akka.kafka.consumer")
+  val consumerSettings =
+    ConsumerSettings(consumerConf, new StringDeserializer, new StringDeserializer)
+      .withBootstrapServers(bootstrapServers)
+      .withGroupId("group1")
+      .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+
+  val cons = Consumer
+    .plainSource(
+      consumerSettings,
+      Subscriptions.assignmentWithOffset((new TopicPartition("random-data", 0), 0))
+    ).runWith(Sink.foreach(println))
+
+  Await.result(cons, Duration.Inf)
+
   system.terminate()
 }
